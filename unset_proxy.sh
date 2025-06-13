017752b1ce45217c9fcfb1856213a3c0870479ff
abstractor404 <244439775@qq.com>
2025-06-13 20:17:04 +0800

    add proxy shell

--- unset_proxy.sh
+++ unset_proxy.sh
@@ -1,5 +1,11 @@
-unset  http_proxy
-unset https_proxy
-unset  ftp_proxy
-unset  no_proxy
-
+ # set proxy config via profie.d - should apply for all users
+export http_proxy=""
+export https_proxy=""
+export ftp_proxy=""
+export no_proxy="127.0.0.1,localhost"
+# For curl
+export HTTP_PROXY=""
+export HTTPS_PROXY=""
+export FTP_PROXY=""
+export NO_PROXY="127.0.0.1,localhost"
+#将要从代理中排除的其他IP添加到NO_PROXY和no_proxy环境变量中
