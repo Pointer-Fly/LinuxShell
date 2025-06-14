6cb260ede09230e000603a45cf631ae9c5b4b15f
abstractor404 <244439775@qq.com>
2025-06-14 09:46:32 +0800

    add echo

--- proxy.sh
+++ proxy.sh
@@ -1,12 +1,13 @@
- # set proxy config via profie.d - should apply for all users
-export http_proxy="http://192.168.3.61:7897/"
-export https_proxy="https://192.168.3.61:7897/"
-export ftp_proxy="http://192.168.3.61:7897/"
+# set proxy config via profile.d - should apply for all users
+IP="192.168.3.61:7897"
+export http_proxy="http://${IP}/"
+export https_proxy="http://${IP}/"
+export ftp_proxy="http://${IP}/"
 export no_proxy="127.0.0.1,localhost"
 # For curl
-export HTTP_PROXY="http://192.168.3.61:7897/"
-export HTTPS_PROXY="http://192.168.3.61:7897/"
-export FTP_PROXY="http://192.168.3.61:7897/"
+export HTTP_PROXY="http://${IP}/"
+export HTTPS_PROXY="https://${IP}/"
+export FTP_PROXY="http://${IP}/"
 export NO_PROXY="127.0.0.1,localhost"
+echo "成功设置代理：${IP}"
 #将要从代理中排除的其他IP添加到NO_PROXY和no_proxy环境变量中
-
--- unset_proxy.sh
+++ unset_proxy.sh
@@ -8,4 +8,5 @@ export HTTP_PROXY=""
 export HTTPS_PROXY=""
 export FTP_PROXY=""
 export NO_PROXY="127.0.0.1,localhost"
+echo "取消代理成功"
 #将要从代理中排除的其他IP添加到NO_PROXY和no_proxy环境变量中
